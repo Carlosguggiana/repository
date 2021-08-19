3f3235e518dbb442f1ea333d3e44ce9984967a40
Carlosguggiana <duduguggiana@gmail.com>
2021-08-19 14:27:26 -0300

    web
    web

---
+++ projeto 2/src/main/java/projeto2/Servletcarlo.java
@@ -0,0 +1,28 @@
+package projeto2;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servletcarlo
+ */
+@WebServlet("/Servletcarlo")
+public class Servletcarlo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servletcarlo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
---
+++ projeto 2/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>aula de web</title>
+</head>
+<body>
+<p><h3> dever 1 </h3>
+</body>
+</html>
+
+
